094327a3b2e335b4f89a4b161bb02ad760bffedd
Quy Ngo <quy.nqhcm@gmail.com>
2023-02-03 01:31:34 +0700

    add client first-time

---
+++ client.py
@@ -0,0 +1,13 @@
+from lzma import FORMAT_ALONE
+import socket
+import json 
+import time 
+from datetime import datetime
+from operator import index
+from tkinter import*
+
+#-----------------CLIENT--------------------
+HOST = '127.0.0.1'
+PORT = 65432
+FORMAT = 'utf8'
+
